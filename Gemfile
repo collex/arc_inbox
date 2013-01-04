9090749b0d5cb1f048bb774f0125933df8e2eae6
Paul Rosen <paul@performantsoftware.com>
2013-01-04 16:17:49 -0500

    Get ready for deployment

--- Gemfile
+++ Gemfile
@@ -13,8 +13,10 @@ group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
 
+  gem 'libv8', '~> 3.11.8' # to make therubyracer compile
+
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
+  gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
 end
--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -3,7 +3,7 @@ EXCEPTION_RECIPIENTS = SITE_SPECIFIC['exception_notifier']['exception_recipients
 EXCEPTION_SENDER = SITE_SPECIFIC['exception_notifier']['sender_address']
 
 if Rails.env.to_s != 'development'
-	Typewright::Application.config.middleware.use ExceptionNotifier,
+	ArcInbox::Application.config.middleware.use ExceptionNotifier,
 		:email_prefix => EXCEPTION_PREFIX,
 		:sender_address => EXCEPTION_SENDER,
 		:exception_recipients => EXCEPTION_RECIPIENTS.split(' ')
